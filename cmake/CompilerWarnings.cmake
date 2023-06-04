e378aacf075ed51807180a1fc035a4f5a7e18cca
yunwei37 <1067852565@qq.com>
2023-06-04 17:42:06 +0800

    remove and fixup cmake

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -40,25 +40,36 @@ function(set_project_warnings project_name)
   )
 
   set(CLANG_WARNINGS
-      -Wall
-      -Wextra  # reasonable and standard
-      -Wshadow # warn the user if a variable declaration shadows one from a
+        -Wall
+        -Wextra  # reasonable and standard
+        -Wshadow # warn the user if a variable declaration shadows one from a
                # parent context
-      -Wnon-virtual-dtor # warn the user if a class with virtual functions has a
+        -Wnon-virtual-dtor # warn the user if a class with virtual functions has a
                          # non-virtual destructor. This helps catch hard to
                          # track down memory errors
-      -Wold-style-cast # warn for c-style casts
-      -Wcast-align     # warn for potential performance problem casts
-      -Wunused         # warn on anything being unused
-      -Woverloaded-virtual # warn if you overload (not override) a virtual
+        -Wold-style-cast # warn for c-style casts
+        -Wcast-align     # warn for potential performance problem casts
+        -Wunused         # warn on anything being unused
+        -Woverloaded-virtual # warn if you overload (not override) a virtual
                            # function
-      -Wpedantic   # warn if non-standard C++ is used
-      -Wconversion # warn on type conversions that may lose data
-      -Wsign-conversion  # warn on sign conversions
-      -Wnull-dereference # warn if a null dereference is detected
-      -Wdouble-promotion # warn if float is implicit promoted to double
-      -Wformat=2 # warn on security issues around functions that format output
+        -Wconversion # warn on type conversions that may lose data
+        # -Wsign-conversion  # warn on sign conversions
+        -Wnull-dereference # warn if a null dereference is detected
+        -Wdouble-promotion # warn if float is implicit promoted to double
+        -Wformat=2 # warn on security issues around functions that format output
                  # (ie printf)
+        -DCONFIG_BPF_JIT 
+        -fno-omit-frame-pointer 
+        -fno-strict-overflow 
+        -fshort-wchar 
+        -fno-strict-aliasing 
+        -fPIE
+        -fno-common 
+        -fno-delete-null-pointer-checks 
+        -fno-optimize-sibling-calls 
+        -fno-stack-check
+        -std=gnu99
+        -Wno-gnu
   )
 
   if (${PROJECT_NAME}_WARNINGS_AS_ERRORS)
--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -3,8 +3,48 @@ file(GLOB srcfiles
 file(GLOB headerfiles
 		"${PROJECT_SOURCE_DIR}/src/*.h")
 
+# Detect the architecture
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "arm")
+  message(STATUS "arm architecture detected")
+  set(ARCH_SOURCES
+    src/arch/arm/bpf_jit_32.c
+  )
+  set(ARCH_HEADERS
+    src/arch/arm/
+  )
+elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
+  message(STATUS "arm64 architecture detected")
+  set(ARCH_SOURCES
+    src/arch/arm64/bpf_jit_comp.c
+    src/arch/arm64/insn.c
+  )
+  set(ARCH_HEADERS
+    src/arch/arm64/
+  )
+elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "riscv")
+  message(STATUS "riscv architecture detected")
+  set(ARCH_SOURCES
+    src/arch/riscv/bpf_jit_comp64.c
+    src/arch/riscv/bpf_jit_core.c
+  )
+  set(ARCH_HEADERS
+    src/arch/riscv/
+  )
+elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "i686" OR CMAKE_SYSTEM_PROCESSOR MATCHES "i386")
+  message(STATUS "x86 architecture detected")
+  set(ARCH_SOURCES
+    src/arch/x86/bpf_jit_comp.c
+  )
+  set(ARCH_HEADERS
+    src/arch/x86/
+  )
+else()
+  message(FATAL_ERROR "Unsupported architecture")
+endif()
+
 set(sources
-${srcfiles}
+  ${ARCH_SOURCES}
+  ${srcfiles}
 )
 
 set(exe_sources
@@ -14,8 +54,10 @@ set(exe_sources
 
 set(headers
     include/
+    ${ARCH_HEADERS}
     ${headerfiles}
 )
+message(STATUS ${headers})
 
 set(test_sources
   src/tmp_test.cpp
